14a14c4a375ff89c2c249eb1389c83edf801b92a
Pietro Sette <psette@usc.edu>
2016-04-14 20:27:29 -0700

    git

--- hw7/examscheduleer.cpp
+++ hw7/examscheduleer.cpp
@@ -5,32 +5,40 @@
 #include <vector>
 #include <cstdlib>
 #include <map>
-#include "avlbst.h"
+/*	isValid takes parameters as a map of classes and a map of the corresponding slots
+*	it iterates through all the classes and sees if a student is in multiple of the same classes
+*	that have the same time slot. If they are in the same slot it returns true
+*	if it iterates through without any collisions, it returns true
+*/
 bool isValid(std::map<std::string,std::vector<int> >& classMap, std::map<int,int>& slotMap){
 	for(std::map<std::string,std::vector<int> >::iterator it = classMap.begin(); it!= classMap.end(); ++it){
 		for(unsigned i = 0; i < it -> second.size(); ++i){
 			for(unsigned j= 0; j < it -> second.size(); ++j){
-				if(i == j ) continue;
-				int i_val = slotMap.find(it->second[i])->second, jval = slotMap.find(it->second[j])->second;
-				if(i_val == jval && slotMap[it->second[i]] != 0 )
+				if(slotMap[(it->second)[i]] == slotMap[(it->second)[j]] && i != j && slotMap[(it->second)[i]] != 0 )
 					return false;
 			}
 		}
 	}
 	return true;
 }
-bool solveHelper(std::map<std::string,std::vector<int> >& classMap, std::map<int,int>& slotMap, const std::string& element, std::vector<int>& classesvec, const int& index, const int& num_slots){
+/*	solve takes the paramters of a map of classes, map of timeslots,the current student,a vecetor of all the classes, index, and how many slots
+*	this is a BT recursive function that returns true when the index of the current class is the same as the size of the class vector
+*	it sets the slot of the current class as every possible iteration and then determines if a pair is valid using the is valid function
+*	if it is valid, and if the next class can also pass through solve then it returns true
+*/	
+bool solve(std::map<std::string,std::vector<int> >& classMap, std::map<int,int>& slotMap, const std::string& element, std::vector<int>& classesvec, const int& index, const int& num_slots){
 	if(index == classesvec.size())	return true;
 	std::map<std::string,std::vector<int> >::iterator it = classMap.find(element);
 	if( index > it->second.size() )
 		if(++it == classMap.end() ) return true;
 	for(int i = 0; i < num_slots; ++i){
-		slotMap.find(classesvec[index])->second = i + 1;
+		slotMap[ classesvec[index] ] = i + 1;
 		if( isValid(classMap,slotMap) )
-			if(solveHelper(classMap,slotMap,it->first,classesvec, index + 1, num_slots)) return true;
+			if(solve(classMap,slotMap,it->first,classesvec, index + 1, num_slots)) return true;
 	}
 	return false;
 }
+//	Main checks if the file is properly formatted then reads the data into maps and vectors
 int main(int argc, char* argv[]){
 	int num_exams = -1, num_students = -1, num_slots = -1, temp_class = 0;
 	std::ifstream input(argv[1]);
@@ -58,9 +66,9 @@ int main(int argc, char* argv[]){
 			if(!isThere) classesvec.push_back(temp_class);
 			vec.push_back(temp_class);
 		}
-		classMap.insert(std::make_pair(student,vec));
+		classMap[student]= vec;
 	}
-	if(solveHelper(classMap, slotMap, classMap.begin()->first, classesvec, 0, num_slots)){
+	if(solve(classMap, slotMap, classMap.begin()->first, classesvec, 0, num_slots)){
 		for(std::map<int,int>::iterator it = slotMap.begin(); it != slotMap.end(); ++it)
 			std::cout<<it->first<<"\t"<<it->second<<std::endl;
 	} else

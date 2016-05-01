b9a848896bb1a0b21a44e7994f675f5074dc1a9f
Pietro Sette <psette@usc.edu>
2016-04-30 17:15:44 -0700

    fuck git

--- hw8/test.cpp
+++ hw8/test.cpp
@@ -1,32 +1,31 @@
 #include "hashtable.h"
 #include "splaytree.h"
-#include <vector>
 #include <ctime>
 #include <fstream>
 int main (int argc, char *argv[]){
-	char * tempa = argv[1];
+	char * tempa = argv[2];
 	std::ifstream input(tempa);
 	std::ifstream input2(tempa);
-	int num = argv[2][0] -'0';
+	int num = argv[1][0] -'0';
 	if(argc != 3 || (num != 4 && num != 5) ){
 		std::cerr<<"Check Input"<<std::endl;
 		return -1;
 	}
 	std::string temp;
-	HashTable hash;
 	SplayTree<std::string,int> splay;
 	clock_t start = clock();
-	while(input>>temp)	hash.add(temp);
-	double hashTime = (clock() - start ) / (double) CLOCKS_PER_SEC;
-	start = clock();
-	std::cerr<<hashTime;
-	temp ="";
-	while(input2>>temp)	splay.add(temp);
-	double splayTime = (clock() - start ) / (double) CLOCKS_PER_SEC;
-	hash.reportAll(std::cout);
-	std::cout<<"HASH TIME: "<<hashTime<<std::endl;
-	std::cout<<"SPLAY TIME: "<<splayTime<<std::endl;
-	if(splayTime > hashTime) std::cout<<"HashTable FASTER BY: "<<splayTime - hashTime<<std::endl;
-	else std::cout<<"SplayTree FASTER BY: "<<hashTime - splayTime<<std::endl;
+	if(num == 4){
+		HashTable hash;
+		while(input >> temp) hash.add(temp);
+		hash.reportAll(std::cout);
+		std::cout<<"HASH TIME: "<< (clock() - start ) / (double) CLOCKS_PER_SEC<<" seconds."<<std::endl;
+
+	}	else {
+		SplayTree<std::string,int> splay;
+		while(input2>>temp)	splay.add(temp);
+		splay.reportAll(std::cout);
+		std::cout<<"SPLAY TIME: "<<(clock() - start ) / (double) CLOCKS_PER_SEC<<" seconds."<<std::endl;
+
+	}
 	return 0;
 }
